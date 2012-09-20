e3ec01077ba540ff87f2b57eb986a90ed757e2a3
Michael Landes <mokonzi131@gmail.com>
2012-09-20 05:16:07 -0400

    added usage statement

--- ml_server/src/main.c
+++ ml_server/src/main.c
@@ -5,20 +5,52 @@
 #include <stdio.h>
 #include <unistd.h>
 #include "server.h"
+#include <string.h>
+#include <assert.h>
+
+static unsigned short int port = 0;
+static char* root = NULL;
+
+static int setArguments(int, char**);
 
 int main(int args, char** argv)
 {
-	int i;
+	int error;
 
-	for (i = 0; i < args; ++i)
+	assert(sizeof(unsigned short int) == 2);
+
+	// parse command line
+	error = setArguments(args, argv);
+	if (error)
 	{
-		printf("%s\n", argv[i]);
+		printf("usage: %s [-p port] [-r root_directory]\n", argv[0]);
+		printf("  [-p port] : port on which to listen (default is 51119)\n");
+		printf("  [-r root_directory] : directory to serve (default is ./)\n\n");
+		return -1;
 	}
 
+	// run the server
+	error = server(port, root);
+	switch(error)
+	{
+		case -1:
+			printf("Server failed unexpectedly\n");
+			break;
+		default:
+			printf("Terminating Server\n");
+	}
 
+	return 0;
+}
 
+static int setArguments(int args, char** argv)
+{
+	int check;
 
-	getopt(0, NULL, NULL);
+	while ((check = getopt(argc, argv, "pr")) != -1)
+	{
+		// TODO
+	}
 
 	return 0;
 }
--- ml_server/src/server.c
+++ ml_server/src/server.c
@@ -4,8 +4,33 @@
 
 #include "server.h"
 
+#include <stdio.h>
+
+/* DATA */
+static char* host = "host";
 static int port = 1019;
 static int ip = 123;
 static char* root = "hello world";
 
+static int initialize();
+static int run();
 
+/* PUBLIC INTERFACE */
+int server(unsigned short int _port, char* _root)
+{
+	printf("port: %d\n", _port);
+	printf("root: %s\n", _root);
+	// TODO
+	return -1;
+}
+
+/* PRIVATE INTERFACE */
+static int initialize(void)
+{
+	printf("init...\n");
+}
+
+static int run(void)
+{
+	printf("run...\n");
+}
