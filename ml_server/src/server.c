44deb33dc3219d1b764afebe9d2f26d074e8a27b
Michael Landes <mokonzi131@gmail.com>
2012-09-20 17:33:35 -0400

    prettier socket framework, ready for http

--- ml_server/src/server.c
+++ ml_server/src/server.c
@@ -12,12 +12,15 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#define SOCKET_ERROR    -1
+#define BACKLOG			5
+
 /* DATA */
 const unsigned short int ml_DEFAULT_PORT_NUMBER = 51115;
 
-static unsigned short int portNumber;
-static char hostMachine[512];
 static char rootDirectory[512];
+static int hServerSocket;
+static struct sockaddr_in ServerAddress;
 
 static int initialize(const unsigned short int, const char*);
 static int run(void);
@@ -51,38 +54,18 @@ int ml_server(const unsigned short int port, const char* root)
 }
 
 /* PRIVATE INTERFACE */
-static int initialize(const unsigned short int _port, const char* _root)
+static int initialize(unsigned short int port, const char* root)
 {
+	int nAddressSize = sizeof(struct sockaddr_in);
+
 	printf("INITIALIZING Server...\n");
 
-	// setup the root directory
-	strcpy(rootDirectory, "hello");
-
-	// setup the host machine
-	strcpy(hostMachine, "localhost");
-
 	// setup the port
-	portNumber = (_port == 0 ? ml_DEFAULT_PORT_NUMBER : _port);
-	assert(portNumber > 0 && portNumber <= 65535);
+	port = (port == 0 ? ml_DEFAULT_PORT_NUMBER : port);
+	assert(port > 0 && port <= 65535);
 
-	printf("   port = %d\n", portNumber);
-	printf("   root = %s\n", rootDirectory);
-
-	return 0;
-}
-
-#define SOCKET_ERROR        -1
-#define BUFFER_SIZE         100
-#define MESSAGE             "This is the message I'm sending back and forth"
-#define QUEUE_SIZE          5
-static int run(void)
-{
-	int hServerSocket; // handles to socket
-	int hSocket;
-	//struct hostent* pHostInfo; // Internet socket address struct
-	struct sockaddr_in Address; // Internet socket address struct
-	int nAddressSize = sizeof(struct sockaddr_in);
-	//char pBuffer[BUFFER_SIZE];
+	// setup the root directory TODO <<<<< ----------------------------------------
+	strcpy(rootDirectory, "TODO");
 
 	// initialize socket
 	hServerSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -93,42 +76,57 @@ static int run(void)
 	}
 
 	// setup socket structure
-	Address.sin_addr.s_addr = htonl(INADDR_ANY);
-	Address.sin_port = htons(portNumber);
-	Address.sin_family = AF_INET;
+	ServerAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+	ServerAddress.sin_port = htons(port);
+	ServerAddress.sin_family = AF_INET;
 
 	// bind to a port
-	printf("Binding to port %d\n", portNumber);
-	if (bind(hServerSocket, (struct sockaddr*)&Address, sizeof(Address)) == SOCKET_ERROR)
+	if (bind(hServerSocket, (struct sockaddr*)&ServerAddress, sizeof(ServerAddress)) == SOCKET_ERROR)
 	{
 		printf("ERROR: Failed to bind socket to port\n");
 		return -1;
 	}
 
-	// report setup
-	getsockname(hServerSocket, (struct sockaddr*)&Address, (socklen_t*)&nAddressSize);
-	printf("Opened socket as fd (%d) on port (%d) for stream i/o\n", hServerSocket, ntohs(Address.sin_port));
-	printf("server\n\
-			sin_family			= %d\n\
-			sin_addr.s_addr		= %s\n\
-			sin_port			= %d\n"
-			, Address.sin_family
-			, inet_ntoa(Address.sin_addr)
-			, ntohs(Address.sin_port));
-
 	// establish a listen queue
-	if (listen(hServerSocket, QUEUE_SIZE) == SOCKET_ERROR)
+	if (listen(hServerSocket, BACKLOG) == SOCKET_ERROR)
 	{
 		printf("ERROR: Server failed to listen\n");
 		return -1;
 	}
+
+	// report setup
+	getsockname(hServerSocket, (struct sockaddr*)&ServerAddress, (socklen_t*)&nAddressSize);
+	printf("  server_ip = %s\n", inet_ntoa(ServerAddress.sin_addr));
+	printf("  port      = %d\n", ntohs(ServerAddress.sin_port));
+	printf("  directory = %s\n", rootDirectory);
+
+	return 0;
+}
+
+#define BUFFER_SIZE         100
+#define MESSAGE             "This is the message I'm sending back and forth"
+static int run(void)
+{
+	int hSocket;
+	static struct sockaddr_in ClientAddress;
+	int nAddressSize = sizeof(struct sockaddr_in);
+
+	//struct hostent* pHostInfo; // Internet socket address struct
+	//char pBuffer[BUFFER_SIZE];
 
 	printf("RUNNING Server...\n");
 
 	while(1)
 	{
 		printf("Waiting for a connection\n");
-		hSocket = accept(hServerSocket, (struct sockaddr*)&Address, (socklen_t*)&nAddressSize);
+		hSocket = accept(hServerSocket, (struct sockaddr*)&ClientAddress, (socklen_t*)&nAddressSize);
+		printf("connection\n\
+			sin_family			= %d\n\
+			sin_addr.s_addr		= %s\n\
+			sin_port			= %d\n"
+			, ClientAddress.sin_family
+			, inet_ntoa(ClientAddress.sin_addr)
+			, ntohs(ClientAddress.sin_port));
 	}
 
 	return 0;
@@ -139,13 +137,9 @@ static int run(void)
 
     for(;;)
     {
-        printf("\nWaiting for a connection\n");
-        // get the connected socket
-        hSocket=accept(hServerSocket,(struct sockaddr*)&Address,(socklen_t *)&nAddressSize);
-
 	printf("\nGot a connection");
         strcpy(pBuffer,MESSAGE);
-	printf("\nSending \"%s\" to client",pBuffer);
+	printf("\nSending \"%s\" tolisten client",pBuffer);
         //number returned by read() and write() is the number of bytes
         // read or written, with -1 being that an error occured
         // write what we received back to the server
@@ -166,5 +160,4 @@ static int run(void)
          return 0;
         }
     }
-}
 */
