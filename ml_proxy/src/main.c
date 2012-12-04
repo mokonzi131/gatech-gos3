2d4107b48ba813ef4a357dd48aa928130f31703b
Michael Landes <mokonzi131@gmail.com>
2012-12-04 02:55:26 -0500

    initial project setup, hello world

--- ml_proxy/src/main.c
+++ ml_proxy/src/main.c
@@ -1,107 +1,108 @@
 /// Michael Landes
-/// GaTech : GOS : Project 2
+/// GaTech : GOS : Project 3
 /// \\\///\\\///\\\///\\\///
-#include "globals.h"
+//#include "globals.h"
 
 #include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <signal.h>
+//#include <unistd.h>
+//#include <stdlib.h>
+//#include <signal.h>
 
-#include "proxy.h"
+//#include "proxy.h"
 
-#define INPUT_PARSE_ERROR		-2
+//#define INPUT_PARSE_ERROR		-2
 
 /// DATA ///
-static unsigned short int port = 0;
-static unsigned int workers = 0;
-static int shared = 0;
+//static unsigned short int port = 0;
+//static unsigned int workers = 0;
+//static int shared = 0;
 
 /// PRIVATE INTERFACE ///
-static int setArgs(int, char**);
-static void h_interrupt(int);
-static void h_pipe(int);
+//static int setArgs(int, char**);
+//static void h_interrupt(int);
+//static void h_pipe(int);
 
 /// MAIN ///
 int main(int argc, char** argv)
 {
-	int result = 0;
+	printf("Hello World\n");
+//	int result = 0;
 
 	// Register signal and signal handler
-	signal(SIGINT, h_interrupt);
-	signal(SIGPIPE, h_pipe);
+//	signal(SIGINT, h_interrupt);
+//	signal(SIGPIPE, h_pipe);
 
 	// parse command line
-	result = setArgs(argc, argv);
-	if (result != SUCCESS)
-	{
-		printf("usage: %s [-p port] [-w workers] [-s]\n", argv[0]);
-		printf("  [-p port] : valid (1-%d) port on which to listen. (default = %d)\n", MAX_PORT, DEFAULT_PROXY_PORT);
-		printf("  [-w workers] : a reasonable (1-50) number of worker threads to use. (default = %d)\n", DEFAULT_PROXY_WORKERS);
-		printf("  [-s] : enable shared mode (efficient proxy server)\n");
-		printf("\n");
-		return result;
-	}
+//	result = setArgs(argc, argv);
+//	if (result != SUCCESS)
+//	{
+//		printf("usage: %s [-p port] [-w workers] [-s]\n", argv[0]);
+//		printf("  [-p port] : valid (1-%d) port on which to listen. (default = %d)\n", MAX_PORT, DEFAULT_PROXY_PORT);
+//		printf("  [-w workers] : a reasonable (1-50) number of worker threads to use. (default = %d)\n", DEFAULT_PROXY_WORKERS);
+//		printf("  [-s] : enable shared mode (efficient proxy server)\n");
+//		printf("\n");
+//		return result;
+//	}
 
 	// launch the proxy server
-	result = ml_proxy(port, workers, shared);
-	switch(result)
-	{
-		case (SUCCESS):
-			break;
-		default:
-			printf("MAIN: [ml_proxy] terminated unexpectedly...\n");
-	}
+//	result = ml_proxy(port, workers, shared);
+//	switch(result)
+//	{
+//		case (SUCCESS):
+//			break;
+//		default:
+//			printf("MAIN: [ml_proxy] terminated unexpectedly...\n");
+//	}
 
-	printf("\n");
-	return result;
+//	printf("\n");
+//	return result;
 }
 
 /// IMPLEMENTATION ///
-static void h_interrupt(int signum)
-{
-	ml_proxy_shutdown();
-}
+//static void h_interrupt(int signum)
+//{
+//	ml_proxy_shutdown();
+//}
 
-static void h_pipe(int signum)
-{
-	return;
-}
+//static void h_pipe(int signum)
+//{
+//	return;
+//}
 
 static int setArgs(int argc, char** argv)
 {
-	int check;
-	int index;
-	int test;
+//	int check;
+//	int index;
+//	int test;
 
 	// set command line inputs, reject a mal-formed input
-	while((check = getopt(argc, argv, "p:w:s")) != ERROR)
-	{
-		switch(check)
-		{
-			case 'p':
-				test = atoi(optarg);
-				if (test <= 0 || test > MAX_PORT)
-					return (INPUT_PARSE_ERROR);
-				port = test;
-				break;
-			case 'w':
-				test = atoi(optarg);
-				if (test <= 0 || test > MAX_WORKERS)
-					return (INPUT_PARSE_ERROR);
-				workers = test;
-				break;
-			case 's':
-				shared = 1;
-				break;
-			case '?':
-			default:
-				return (INPUT_PARSE_ERROR);
-		}
-	}
+//	while((check = getopt(argc, argv, "p:w:s")) != ERROR)
+//	{
+//		switch(check)
+//		{
+//			case 'p':
+//				test = atoi(optarg);
+//				if (test <= 0 || test > MAX_PORT)
+//					return (INPUT_PARSE_ERROR);
+//				port = test;
+//				break;
+//			case 'w':
+//				test = atoi(optarg);
+//				if (test <= 0 || test > MAX_WORKERS)
+//					return (INPUT_PARSE_ERROR);
+//				workers = test;
+//				break;
+//			case 's':
+//				shared = 1;
+//				break;
+//			case '?':
+//			default:
+//				return (INPUT_PARSE_ERROR);
+//		}
+//	}
 
-	for (index = optind; index < argc; ++index)
-		return (INPUT_PARSE_ERROR);
+//	for (index = optind; index < argc; ++index)
+//		return (INPUT_PARSE_ERROR);
 
-	return (SUCCESS);
+//	return (SUCCESS);
 }
